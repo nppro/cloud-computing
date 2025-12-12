ea7394bd2d1e53132c8967efaeafe79d1e21e9b5
Phuoc Pham <phamnguyenphuoc@gmail.com>
2025-12-12 13:13:54 +0100

    Initial commit

---
+++ src/main/java/com/cloud_computing/campus_core/CampusCoreApplication.java
@@ -0,0 +1,13 @@
+package com.cloud_computing.campus_core;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CampusCoreApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CampusCoreApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/cloud_computing/campus_core/CampusCoreApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cloud_computing.campus_core;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CampusCoreApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
